3eff1f27ed0768e4662df23bb499a6818b321310
tHALL3000 <tiffanyhall.co@gmail.com>
2021-09-25 21:05:52 -0600

    !sound added, current time added to reset sound, added key function for lights when "playing"

--- pages/main.js
+++ pages/main.js
@@ -4,6 +4,14 @@ window.addEventListener("keydown", function (e) {
 			data-key="${e.keyCode}"
 		]`
 	);
+	const key = document.querySelectorAll(
+		`audio[
+			data-key="${e.keyCode}"
+		]`
+	);
+	if (!sound) return;
+	sound.currentTime = 0;
 	sound.play();
+	key.classList.add("playing");
 	console.log(sound);
 });
